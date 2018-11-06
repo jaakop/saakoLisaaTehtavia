ba526213e7e16117487848b95639ce6d22737bb4
Jaakko <jaakko@kronogra.fi>
2018-11-06 17:42:26 +0200

    Cleaning up some more

--- tehtavia/tehtavia.js
+++ tehtavia/tehtavia.js
@@ -1,5 +1,5 @@
-
-function lisaaTehtavia(){
+function lisaaTehtavia()
+{
     document.getElementById("lisaa").style.opacity = 1;
     setInterval(function(){ document.getElementById("lisaa").style.opacity = 0; }, 10000);
 }
