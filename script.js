970a2551e1bb7bd153ee7ea886a05be93296a1be
Jaakko <jaakko@kronogra.fi>
2018-11-07 12:13:12 +0200

    Made the page to scroll to the top smoothly

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onscroll = function() {scrollFunction()};
 
+var speed = 1;
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("myBtn").style.display = "block";
@@ -8,6 +9,21 @@ function scrollFunction() {
     }
 }
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    speed = 1;
+    console.log( document.body.scrollTop,document.documentElement.scrollTop);
+    scroll();
+    console.log(speed);
+}
+function scroll(){
+    if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){   
+
+        document.body.scrollTop = document.body.scrollTop - speed/100;
+        document.documentElement.scrollTop = document.documentElement.scrollTop - speed/100;
+        speed = speed + 15;
+        console.log(speed);
+        if(speed > 2000)
+            speed = 2000;
+        setTimeout(scroll, 1);
+    } 
+    
 }
